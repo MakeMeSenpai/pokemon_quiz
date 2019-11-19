2b0745137ee3102fb8f538f867eb847319112342
Anthony <sean.a.protho1@gmail.com>
2019-11-19 10:57:26 -0800

    print statements

--- app.py
+++ app.py
@@ -1,34 +1,42 @@
 # Subject: What's that pokemon?!
 print("What's that Pokemon?!? \n"
-"Do you know them all? ")
-# Question: Which pokemon is a rat, with a chubby belly (in the original card game)?
+"Do you know them all? \n"
+"Answer by entering a b c d or e")
 # Answer: It's Pikachu! 
-# Choice: It's Rattata!
-# Question: Which pokemon has non-stop headaches?
+print("Which pokemon is a rat, with a chubby belly (in the original card game)? \n"
+"A. It's Pikachu! \n"
+"B. It's Rattata! \n")
 # Answer: It's Psyduck!
-# Choice: It's Porygon!
-# Question: which pokemon was NOT released in Black and White?
+print("Which pokemon has non-stop headaches? \n"
+"A. It's Psyduck! \n"
+"B. It's Porygon! \n"
+)
 # Answer: It's Solgaleo!
-# Choice: It's Zekrom!
-# Choice: It's Reshiram!
-# Question: Based off stats, who was the best pokemon to choose in the beggining of Pokemon Red and Blue?
+print("which pokemon was NOT released in Black and White? \n"
+"A. It's Zekrom! \n"
+"B. It's Solgaleo! \n"
+"C. It's Reshiram! \n")
 # Answer: It's Bulbasaur!
-# Choice: It's Charmander!
-# choice: It's Squirtle!
-# Question: Which pokemon has the most options when it comes to evolution? 
-# Answer: It's Eevee!
-# Choice: It's Panpour!
-# Choice: It's Pansear!
-# Choice: It's Pansage!
-# Question: When exposed to a leaf stone, what does Vileplume evolve into?
+print("Based off stats, who was the best pokemon to choose in the beggining of Pokemon Red and Blue? \n"
+"A. It's Squirtle! \n"
+"B. It's Charmander! \n"
+"C. It's Bulbasaur! \n")
+#Answer: It's Eevee!
+print("Which pokemon has the most options when it comes to evolution?  \n"
+"A. It's Pansear! \n"
+"B. It's Panpour! \n"
+"C. It's Pansage! \n"
+"D. It's Eevee! \n")
 # Answer: It's Gloom!
-# Choice: It's Oddish!
-# Choice: It's Bellossom!
-# choice: It's Carnivine!
-# Question: Which pokemon is the cuttests?
+print("When exposed to a leaf stone, what does Vileplume evolve into? \n"
+"A. It's Bellossom! \n"
+"B. It's Carnivine! \n"
+"C. Gloom \n"
+"D. Oddish \n")
 # Answer: They are all right (all tiny pokemon are cute, fight me)!
-# Choice: It's Jigglypuff!
-# Choice: Is's Eevee!
-# Choice: It's Buneary!
-# Choice: It's Minccino!
-# Choice: It's Rockruff!
+print("Which pokemon is the cuttests? \n"
+"A. It's Jigglypuff! \n"
+"B. Is's Eevee! \n"
+"C. It's Buneary! \n"
+"D. It's Minccino! \n"
+"E. It's Rockruff! \n")
