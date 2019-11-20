cb17ace09b4b6e2585abedc1ca55627fc2aff164
Anthony <sean.a.protho1@gmail.com>
2019-11-20 14:26:26 -0800

    done *yawn*

--- app.py
+++ app.py
@@ -10,67 +10,171 @@ class quiz():
         
 
     def q1(self):
-        # Answer: It's Pikachu! 
-        while True:
+        a = True
+        while a == True:
             y = input("Which pokemon is a rat, with a chubby belly (in the original card game)? \n"
             "A. It's Pikachu! \n"
             "B. It's Rattata! \n")
             z = y.lower()
-            if z == True and z == "a":
+            if z == "a":
                 self.score += 1
-                return False
-            elif z == True and z == "b":
-                return False
+                print("It's Pikachu!")
+                a = False
+            elif z == "b":
+                print("It's Pikachu!")
+                a = False
             else:
                 print("Sorry, what was that?")
-                return True
+                a = True
 
 
     def q2(self):
-        # Answer: It's Psyduck!
-        y = input("Which pokemon has non-stop headaches? \n"
-        "A. It's Psyduck! \n"
-        "B. It's Porygon! \n"
-        )
+        a = True
+        while a == True:
+            y = input("Which pokemon has non-stop headaches? \n"
+            "A. It's Psyduck! \n"
+            "B. It's Porygon! \n")
+            z = y.lower()
+            if z == "a":
+                self.score += 1
+                print("It's Psyduck!")
+                a = False
+            elif z == "b":
+                print("It's Psyduck!")
+                a = False
+            else:
+                print("Sorry, what was that?")
+                a = True
 
     def q3(self):
-        # Answer: It's Solgaleo!
-        y = input("which pokemon was NOT released in Black and White? \n"
-        "A. It's Zekrom! \n"
-        "B. It's Solgaleo! \n"
-        "C. It's Reshiram! \n")
+        a = True
+        while a == True:
+            y = input("which pokemon was NOT released in Black and White? \n"
+            "A. It's Zekrom! \n"
+            "B. It's Solgaleo! \n"
+            "C. It's Reshiram! \n")
+            z = y.lower()
+            if z == "a":
+                print("It's Solgaleo!")
+                a = False
+            elif z == "b":
+                print("It's Solgaleo!")
+                self.score += 1
+                a = False
+            elif z == "c":
+                print("It's Solgaleo!")
+                a = False
+            else:
+                print("Sorry, what was that?")
+                a = True
 
     def q4(self):
-        # Answer: It's Bulbasaur!
-        y = input("Based off stats, who was the best pokemon to choose in the beggining of Pokemon Red and Blue? \n"
-        "A. It's Squirtle! \n"
-        "B. It's Charmander! \n"
-        "C. It's Bulbasaur! \n")
+        a = True
+        while a == True:
+            y = input("Based off stats, who was the best pokemon to choose in the beggining of Pokemon Red and Blue? \n"
+            "A. It's Squirtle! \n"
+            "B. It's Charmander! \n"
+            "C. It's Bulbasaur! \n")
+            z = y.lower()
+            if z == "a":
+                print("It's Bulbasaur!")
+                a = False
+            elif z == "b":
+                print("It's Bulbasaur!")
+                a = False
+            elif z == "c":
+                print("It's Bulbasaur!")
+                self.score += 1
+                a = False
+            else:
+                print("Sorry, what was that?")
+                a = True
 
     def q5(self):
-        #Answer: It's Eevee!
-        y = input("Which pokemon has the most options when it comes to evolution?  \n"
-        "A. It's Pansear! \n"
-        "B. It's Panpour! \n"
-        "C. It's Pansage! \n"
-        "D. It's Eevee! \n")
+        a = True
+        while a == True:
+            y = input("Which pokemon has the most options when it comes to evolution?  \n"
+            "A. It's Pansear! \n"
+            "B. It's Panpour! \n"
+            "C. It's Pansage! \n"
+            "D. It's Eevee! \n")
+            z = y.lower()
+            if z == "a":
+                print("It's Eevee!")
+                a = False
+            elif z == "b":
+                print("It's Eevee!")
+                a = False
+            elif z == "c":
+                print("It's Eevee!")
+                a = False
+            elif z == "d":
+                print("It's Eevee!")
+                self.score += 1
+                a = False
+            else:
+                print("Sorry, what was that?")
+                a = True
 
     def q6(self):
-        # Answer: It's Gloom!
-        y = input("When exposed to a leaf stone, what does Vileplume evolve into? \n"
-        "A. It's Bellossom! \n"
-        "B. It's Carnivine! \n"
-        "C. Gloom \n"
-        "D. Oddish \n")
+        a = True
+        while a == True:
+            y = input("When exposed to a leaf stone, what does Vileplume evolve into? \n"
+            "A. It's Bellossom! \n"
+            "B. It's Carnivine! \n"
+            "C. Gloom \n"
+            "D. Oddish \n")
+            z = y.lower()
+            if z == "a":
+                print("It's Gloom!")
+                a = False
+            elif z == "b":
+                print("It's Gloom!")
+                a = False
+            elif z == "c":
+                print("It's Gloom!")
+                self.score += 1
+                a = False
+            elif z == "d":
+                print("It's Gloom!")
+                a = False
+            else:
+                print("Sorry, what was that?")
+                a = True
 
     def q7(self):
-        # Answer: They are all right (all tiny pokemon are cute, fight me)!
-        y = input("Which pokemon is the cuttests? \n"
-        "A. It's Jigglypuff! \n"
-        "B. Is's Eevee! \n"
-        "C. It's Buneary! \n"
-        "D. It's Minccino! \n"
-        "E. It's Rockruff! \n")
+        a = True
+        while a == True:
+            y = input("Which pokemon is the cuttests? \n"
+            "A. It's Jigglypuff! \n"
+            "B. Is's Eevee! \n"
+            "C. It's Buneary! \n"
+            "D. It's Minccino! \n"
+            "E. It's Rockruff! \n")
+            z = y.lower()
+            if z == "a":
+                print("They are all right (all tiny pokemon are cute, fight me)!")
+                self.score += 1
+                a = False
+            elif z == "b":
+                print("They are all right (all tiny pokemon are cute, fight me)!")
+                self.score += 1
+                a = False
+            elif z == "c":
+                print("They are all right (all tiny pokemon are cute, fight me)!")
+                self.score += 1
+                a = False
+            elif z == "d":
+                print("They are all right (all tiny pokemon are cute, fight me)!")
+                self.score += 1
+                a = False
+            elif z == "e":
+                print("They are all right (all tiny pokemon are cute, fight me)!")
+                self.score += 1
+                a = False
+            else:
+                print("Sorry, what was that?")
+                a = True
 
 def run():
     x = quiz()
